a1f6a3fcfb1ba4dfac761537699d1638f19832f8
rvishnub <rvishnub@yahoo.com>
2016-08-25 07:16:08 -0500

    added new project

---
+++ GenericList/GenericListClass.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class GenericListClass<T> : IEnumerable, IFormattable
+    {
+
+        public T[] genericArray;
+        int position = 0;
+
+        public GenericListClass(T[] item)
+        {
+            genericArray = item;
+        }
+
+        public Array Add(T item)
+        {
+            T[] newGenericArray = new T[genericArray.Length + 1];
+            if (genericArray.Length == 0)
+            {
+                newGenericArray = new T[1] { item };
+            }
+            else
+            {
+                int j = genericArray.Length;
+
+                for (int i = 0; i < j; i++)
+                {
+                    newGenericArray[i] = genericArray[i];
+                }
+                newGenericArray[j] = item;
+                Console.WriteLine(newGenericArray[j]);
+            }
+            genericArray = newGenericArray;
+            return genericArray;
+
+        }
+
+
+        public Array Remove(T item)
+        {
+            int k = 0;
+            for (int i = 0; i < genericArray.Length; i++)
+
+            {
+                if (genericArray[i].Equals(item))
+                {
+                    k++;
+                }
+
+            }
+            T[] newGenericArray = new T[genericArray.Length - k];
+
+            int j = 0;
+            for (int i = 0; i < genericArray.Length; i++)
+            {
+
+                if (genericArray[i].Equals(item))
+                {
+
+                }
+                else
+                {
+                    newGenericArray[j] = genericArray[i];
+                    j++;
+                }
+            }
+            genericArray = newGenericArray;
+            return genericArray;
+        }
+
+        public void PrintList()
+        {
+            for (int i = 0; i < genericArray.Length; i++)
+            {
+                Console.WriteLine(genericArray[i]);
+
+            }
+        }
+        public int Count()
+        {
+            int j = genericArray.Length;
+            return j;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < genericArray.Length; i++)
+            {
+                yield return genericArray[i];
+            }
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override String ToString()
+        {
+                return ToString("G");
+        }
+
+        public void PrintString()
+        {
+            foreach (T item in genericArray)
+            {
+                Console.Write(item.ToString());
+            }
+        }
+
+        private string ToString(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+       
+    }
+}
+
+
---
+++ GenericList/Program.cs
@@ -0,0 +1,36 @@
+﻿using GenericList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GenericListClass<int> testList = new GenericListClass<int>(new int[10] {1,2,3, 2, 2, 4, 5, 6, 7, 8});
+
+            testList.Add(15);
+            testList.PrintList();
+            Console.ReadLine();
+            testList.Remove(2);
+            testList.PrintList();
+            Console.ReadLine();
+            Console.WriteLine(testList.Count());
+
+            foreach (int item in testList)
+            {
+                Console.WriteLine("Hello World");
+                Console.ReadLine();
+            }
+
+            testList.PrintString();
+            Console.ReadLine();
+
+        }
+    }
+}
